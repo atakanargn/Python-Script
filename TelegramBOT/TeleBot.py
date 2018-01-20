843643886b2fb0d918ba8f23ea990a0cb190d341
Atakan Argın <argin.atakan@gmail.com>
2018-01-20 16:08:16 +0300

    Telegram Botu...

---
+++ TelegramBOT/TeleBot.py
@@ -0,0 +1,40 @@
+from telegram.ext import Updater, CommandHandler
+
+# BotFather tarafından verilen API_KEY ile bota bağlandık
+updater = Updater('API_KEY')
+
+# Komutlar
+
+# /merhaba
+def merhaba(bot, update):
+    update.message.reply_text(
+        'Merhaba {}'.format(update.message.from_user.first_name)+', ben TelegramBot scripti Atakan Argın tarafından yazıldım !'+
+        '\n"/yardim" yazarak yardım alabilirsin.')
+
+# /yardim
+def yardim(bot, update):
+    # BEN YANIT OLARAK GÖNDERİLECEK
+    # VERİYİ DOSYADAN ÇEKTİM
+    # SİZ DAAH FARKLI YÖNTEMLER KULLANABİLİRSİNİZ.
+    dosya=open("yardim.txt","r")
+
+    # YANIT
+    update.message.reply_text(dosya.read())
+
+"""
+def yeniKomut(bot, update):
+
+    # YAPILMASINI ISTEDIĞINIZ KOMUTLAR
+    # BURAYA YAZILACAK
+
+    update.message.reply_text("YANIT OLARAK GÖNDERİLECEK VERİ BURAYA YAZILACAK!")
+"""
+
+# KOMUTLAR
+updater.dispatcher.add_handler(CommandHandler('merhaba', merhaba))
+updater.dispatcher.add_handler(CommandHandler('yardim', yardim))
+# updater.dispatcher.add_handler(CommandHandler('yeniKomut', yeniKomut))
+
+# BOTU BAŞLATTIK
+updater.start_polling()
+updater.idle()
